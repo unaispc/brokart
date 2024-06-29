72c1bbe627f2d4a2b32c8ba4fe04623008a2ce97
Unais <unaispc00@gmail.com>
2024-06-29 16:18:18 +0530

    Models for prodects is ready

--- brokart/products/views.py
+++ brokart/products/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+# MODEL FOR PRODECTS
+
+class product(models.MODEL):
+    LIVE=1
+    DELETE=0
+    DELETE_CHOICES=((LIVE,'Live'),(DELETE,'Delete'))
+    title=models.CharField(max_length=200)
+    price=models.FloatField()
+    description=models.TextField()
+    image=models.ImageField(upload_to='/media')
+    priority=models.IntegerField(default=0)
+    delete_status=models.IntegerField(choices=DELETE_CHOICES,default=LIVE)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    
